181de1e5c6bd116a9db02a5a4d07cb164cef8629
cyberkru <74045103+cyberkru@users.noreply.github.com>
2021-07-06 15:32:32 +0700

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-trufflehog --json --regex --entropy=False file:///proj > report.json
-echo report.json
+echo "DojoIP: $DOJOIP"
+
+echo "Trufflehog scanning..."
+trufflehog --json --regex --entropy=False file:///proj > trufflehog-report.json
+printf "\nDone\n"
+
+echo "Sending to Dojo..."
+curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=Trufflehog Scan" -F 'file=@./trufflehog-report.json' --url "http://${DOJOIP}/api/v2/import-scan/"
+printf "\nDone\n"
