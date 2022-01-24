8623406314c0995aa98430a0bc98e63432862bb7
cyberkru <74045103+cyberkru@users.noreply.github.com>
2022-01-24 17:59:52 +0700

    Update scan.sh
    .

--- scan.sh
+++ scan.sh
@@ -9,6 +9,6 @@ printf "\nDone\n"
 echo "Sending to Dojo..."
 PRODID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "${DOJOURL}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
 EGID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "${DOJOURL}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
-curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=Trufflehog Scan" -F 'file=@./trufflehog-report.json' --url "${DOJOURL}/api/v2/import-scan/"
+curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "close_old_findings=true" -F "scan_type=Trufflehog Scan" -F 'file=@./trufflehog-report.json' --url "${DOJOURL}/api/v2/import-scan/"
 
 printf "\nDone\n"
