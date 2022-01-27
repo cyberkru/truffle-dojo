75ec8ee074f05ee2a533c7aa089d985694bdf3e0
cyberkru <74045103+cyberkru@users.noreply.github.com>
2022-01-27 15:02:01 +0700

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -8,7 +8,7 @@ printf "\nDone\n"
 
 echo "Sending to Dojo..."
 PRODID=$(curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "${DOJOURL}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
-EGID=$(curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "${DOJOURL}/api/v2/engagements/?limit=1000" | jq -c '[.results[] | select(.product == ${PRODID} and .engagement_type == "CI/CD")][0] | .id')
+EGID=$(curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "${DOJOURL}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})]" | jq  -c '[.[] | select(.engagement_type == "CI/CD")][0] | .id')
 #EGID=$(curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "${DOJOURL}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
 curl -k -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "close_old_findings=true" -F "scan_type=Trufflehog Scan" -F 'file=@./trufflehog-report.json' --url "${DOJOURL}/api/v2/import-scan/"
 
